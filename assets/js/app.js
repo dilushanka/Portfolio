3d7c41cde10ed946bea1351c586d7bde97174510
Chathura Dilushanka <90453201+dilushanka@users.noreply.github.com>
2023-02-21 12:54:34 +0530

    removed .html

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ document.body.appendChild(script);
 
 let header = $(`
 <nav class="navbar navbar-expand-lg fixed-top dark-theme" id="navbar">
-<a class="navbar-brand" href="index.html">John Doe </a>
+<a class="navbar-brand" href="https://dilu.helalk.com/">John Doe </a>
 <div class="hamburger_wrapper navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
   <div id="js-hamburger" class="hamburger">
@@ -23,11 +23,11 @@ let header = $(`
 
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://dilu.helalk.com/">Home</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="experience">Experience</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects">Projects</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="research">Research</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="education">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://helalk.com/" target="_blank">Blogs</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
